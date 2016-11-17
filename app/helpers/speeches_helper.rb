08c2db139e6edf976c5cf0c2d409f0a0288f4c91
rafeeq-123 <rafeeq-123@hotmail.com>
2016-11-17 07:05:22 -0800

    cleaning up political stats method

--- app/helpers/speeches_helper.rb
+++ app/helpers/speeches_helper.rb
@@ -1,24 +1,9 @@
 module SpeechesHelper
   def political_percents
-    binding.pry
-    string_numbers = @speech.political_stats.gsub(/[\s aocservtivenLbClG{}=>""]/ , "").split(",")
+    string_numbers = @speech.political_stats.gsub(/[\s a-zA-Z{}=>""]/ , "").split(",")
     @liberatian = string_numbers[0].to_f
     @green = string_numbers[1].to_f
     @liberal = string_numbers[2].to_f
     @converative = string_numbers[3].to_f
-    # need to find a way to make this nice!
   end
-
-  # def index_logic
-  #   binding.pry
-  #   @speeches.each do |speech|
-  #     content_tag(:strong,speech.name)
-  #     if speech.text_of_uploaded_content.nil?
-  #       "No content available"
-  #     else
-  #       speech.text_of_uploaded_content
-  #       content_tag :label do speech.political_stats
-  #       end
-  #     end
-  #   end
-   end
+end
