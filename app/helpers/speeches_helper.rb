63abb13de67c2bc279f49cdd0977a08a5f2682dc
rafeeq-123 <rafeeq-123@hotmail.com>
2016-11-18 11:02:56 -0800

    added a not so dry helper method that goes a not so dry graph

--- app/helpers/speeches_helper.rb
+++ app/helpers/speeches_helper.rb
@@ -5,8 +5,17 @@ module SpeechesHelper
     @green = string_numbers[1].to_f
     @liberal = string_numbers[2].to_f
     @converative = string_numbers[3].to_f
+    #I hate this!
   end
-  # def index_political_percent_helper(speech)
-  #   speech.gsub(/[\s a-zA-Z{}=>""]/ , "").split(",")
-  # end
+  def index_political_percent_helper
+    @speeches.each do |speech|
+      formated_string = speech.political_stats.gsub(/[\s a-zA-Z{}=>""]/ , "")
+      formated_stats = formated_string.split(',')
+      @liberatian = formated_stats[0]
+      @green = formated_stats[1]
+      @liberal = formated_stats[2]
+      @converative = formated_stats[3]
+    #I hate this even more!!
+    end
+  end
 end
