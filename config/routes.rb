02021cf9cfcbb9a6cdcf65957a3dba6f915c911b
rafeeq-123 <rafeeq-123@hotmail.com>
2016-11-18 21:32:59 -0800

    adding comments routes and template

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :comments, only:[:index]
   resources :users, only:[:index]
       resources :speeches, only:[:index, :create, :new, :show]
     root 'users#index'
     get 'search', to: 'search#search'
+
     #going to use cancan for nested resources
 end
