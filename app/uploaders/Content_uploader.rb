7d8a9fef2fdfb9ee12ec635d55847629a86b1886
rafeeq-123 <rafeeq-123@hotmail.com>
2017-01-03 20:31:29 -0800

    fixing model

--- app/uploaders/Content_uploader.rb
+++ app/uploaders/Content_uploader.rb
@@ -52,11 +52,4 @@ class ContentUploader < CarrierWave::Uploader::Base
     end
 
 
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
-
 end
