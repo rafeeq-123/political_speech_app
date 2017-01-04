20f9b35bd07cd6c3209dc9b4e5493bcaac99a51c
rafeeq-123 <rafeeq-123@hotmail.com>
2017-01-03 21:30:19 -0800

    adding info in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  INDICO_API = Rails.application.secrets.INDICO_API
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
